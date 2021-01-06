92b650b1824086c154551d1cb0457880240263ae
king <jinpengxiangln@163.com>
2021-01-06 15:04:36 +0800

    gray-test

--- banana-gateway/src/main/java/com/banana/gateway/GatewayApplication.java
+++ banana-gateway/src/main/java/com/banana/gateway/GatewayApplication.java
@@ -1,29 +1,34 @@
 package com.banana.gateway;
 
-import com.banana.gateway.filter.UrlFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})//(scanBasePackages = Constant.COM_KITEDGE, exclude = {DataSourceAutoConfiguration.class})
-@EnableFeignClients//(basePackages = Constant.COM_KITEDGE)
-@EnableConfigurationProperties(Properties.class)
+//@EnableFeignClients//(basePackages = Constant.COM_KITEDGE)
+//@EnableConfigurationProperties(Properties.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public RequestTimeFilter requestTimeFilter() {
-        return new RequestTimeFilter();
-    }
+//    @Bean
+//    public RequestTimeFilter requestTimeFilter() {
+//        return new RequestTimeFilter();
+//    }
+//
+//    @Bean
+//    public UrlFilter tokenFilter() {
+//        return new UrlFilter();
+//    }
 
     @Bean
-    public UrlFilter tokenFilter() {
-        return new UrlFilter();
+    public KeyResolver uriKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getURI().getPath());
     }
 }
